a97e6f8f8b1821e97ea207262b4ea605fd46d5e1
Nirja Shah <nirja_shah@intuit.com>
2017-10-23 13:42:20 -0700

    Added back button in preview

--- src/components/PreviewComponent.js
+++ src/components/PreviewComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import '../style/PreviewComponent.css';
+import { browserHistory } from 'react-router';
 
 /**
  * PreviewComponent renders tatal amount
@@ -18,41 +19,46 @@ class PreviewComponent extends Component {
     });
 
     return (
-             <div className="invoice-box">
-                  <h3>Your Invoice</h3>
-                  <table>
-                    <tbody>
-                      <tr className="customerName">
-                          <th>Customer Name: {customerInfo.customerName}</th>
-                      </tr>
-                      <tr className="customerEmail">
-                          <th>Customer Email: {customerInfo.customerEmail}</th>
-                      </tr>
-                      <tr className="invoiceDate">
-                          <th>Date: {date}</th>
-                      </tr>
-                      <tr>
-                          <th>Line#</th>
-                          <th>Description</th>
-                          <th>Amount</th>
-                      </tr>
-                      {
-                        lineItems &&
-                        lineItems.map(lineItem => (
-                          <tr key={lineItem.lineItemID} className="lineItem">
-                            <td>{lineItem.lineItemID}</td>
-                            <td>{lineItem.lineDescription}</td>
-                            <td>{lineItem.lineAmount}</td>
+             <div>
+                 <div>
+                     <button className='back-button'
+                             onClick={browserHistory.goBack}>Back</button>
+                 </div>
+                 <div className='invoice-box'>
+                      <h3>Your Invoice</h3>
+                      <table>
+                        <tbody>
+                          <tr className='customerName'>
+                              <th>Customer Name: {customerInfo.customerName}</th>
                           </tr>
-                      ))
-                      }
-                      <tr className="total">
-                          <th>Total: {totalAmount}</th>
-                      </tr>
-                    </tbody>
-                  </table>
-
-             </div>
+                          <tr className='customerEmail'>
+                              <th>Customer Email: {customerInfo.customerEmail}</th>
+                          </tr>
+                          <tr className='invoiceDate'>
+                              <th>Date: {date}</th>
+                          </tr>
+                          <tr>
+                              <th>Line#</th>
+                              <th>Description</th>
+                              <th>Amount</th>
+                          </tr>
+                          {
+                            lineItems &&
+                            lineItems.map(lineItem => (
+                              <tr key={lineItem.lineItemID} className='lineItem'>
+                                <td>{lineItem.lineItemID}</td>
+                                <td>{lineItem.lineDescription}</td>
+                                <td>{lineItem.lineAmount}</td>
+                              </tr>
+                          ))
+                          }
+                          <tr className='total'>
+                              <th>Total: {totalAmount}</th>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+               </div>
        )
   }
 }
